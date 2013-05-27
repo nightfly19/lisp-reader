c87215853ce6e59cefc25282157a5d2db6814a2a
Sage Imel <sage@sagenite.net>
2013-05-26 19:57:11 -0700

    Didn't break it

--- index.js
+++ index.js
@@ -1,48 +1,13 @@
-var reader = exports;
-
-var Symbol = reader.Symbol = function(name){
-    this.name = name;
-};
-
-var symbolEquals = reader.symbolEquals = function(a, b){
-    return typeof a == 'object' &&
-        typeof b == 'object' &&
-        a instanceof reader.Symbol &&
-        b instanceof reader.Symbol &&
-        a.name == b.name;
-};
-
-var reader_macros = reader.reader_macros = [];
-
-var addReaderMacro = reader.addReaderMacro = function(name, rule, callback){
-    var new_reader_macro = {name: name,
-                            rule: rule,
-                            callback: callback}
-    reader_macros.push(new_reader_macro);
-    return reader;
-};
-
-var updateReaderMacro = reader.updateReaderMacro = function(name, rule, callback){
-    for (var rule in reader_macros){
-        var reader_macro = reader_macros[rule];
-        if( reader_macro.name = name){
-            reader_macro.rule = rule;
-            reader_macro.callback = callback;
-            return reader;
-        }
-    };
-    return addReaderMacro(name, rule, callback);
-};
-
-var searchForToken = function (token_rule,input_string){
-    var search = '';
-    var tail = input_string;
-    var last_tail = '';
-    var match;
-    var last_match;
-    var began_matching = false;
-    var stopped_matching = false;
-    var eof = false;
+//Should become it's own module
+var tokenHunter = function (token_rule,input_string){
+    var search = '',
+    tail = input_string,
+    last_tail = '',
+    match,
+    last_match,
+    began_matching = false,
+    stopped_matching = false,
+    eof = false;
     while (!stopped_matching && tail.length > 0){
         search = search + tail[0];
         last_tail = tail;
@@ -68,44 +33,89 @@ var searchForToken = function (token_rule,input_string){
     }
 };
 
-var emptyList = reader.emptyList = function(){
+module.exports = function(){};
+
+var Reader = module.exports;
+
+Reader.prototype.Symbol = function(name){
+    this.name = name;
+};
+
+Reader.prototype.symbolEquals = function(a, b){
+    return typeof a == 'object' &&
+        typeof b == 'object' &&
+        a instanceof this.Symbol &&
+        b instanceof this.Symbol &&
+        a.name == b.name;
+};
+
+Reader.prototype.reader_macros = [];
+
+Reader.prototype.addReaderMacro = function(name, rule, callback){
+    var new_reader_macro = {name: name,
+                            rule: rule,
+                            callback: callback(this)}
+    this.reader_macros = this.reader_macros.slice(0);
+    this.reader_macros.push(new_reader_macro);
+    return this;
+};
+
+Reader.prototype.updateReaderMacro = function(name, rule, callback){
+    this.reader_macros = this.reader_macros.slice(0);
+    for (var i in this.reader_macros){
+        var reader_macro = this.reader_macros[i];
+        if( reader_macro.name = name){
+            this.reader_macros[rule] = {name: name,
+                                        rule: rule,
+                                        callback: callback(this)};
+            return this;
+        }
+    }
+    return this.addReaderMacro(name, rule, callback);
+};
+
+Reader.prototype.emptyList = function(){
     return [];
 };
 
-var listCons = reader.listCons = function(list, element){
+Reader.prototype.listCons = function(list, element){
     list.push(element);
     return list;
 };
 
-addReaderMacro('eof', /^$/, function(input_string){
-    return {tail:input_string, error: 'eof'}
-});
-
-addReaderMacro('whitespace', /^[\s,]/, function(){
-    var whitespace_rule = /^[\s,]+$/;
+Reader.prototype.addReaderMacro('eof', /^$/, function(){
     return function(input_string){
-        var token = searchForToken(whitespace_rule, input_string);
-        return readFromString(token.tail);
+        return {tail:input_string, error: 'eof'}
     };
-}());
-
-addReaderMacro('closing_paren', /^\)/, function(input_string){
-    return {value: new Symbol(')'), tail:input_string.substring(1)}
 });
 
-addReaderMacro('opening_paren', /^\(/, function(){
-    var closing_paren = new Symbol(')');
+Reader.prototype.addReaderMacro('whitespace', /^[\s,]/, function(){
+    var whitespace_rule = /^[\s,]+$/;
+    return function(input_string, reader){
+        var token = tokenHunter(whitespace_rule, input_string);
+        return reader.readFromString(token.tail);
+    };
+});
+
+Reader.prototype.addReaderMacro('closing_paren', /^\)/, function(reader){
     return function(input_string){
+        return {value: new reader.Symbol(')'), tail:input_string.substring(1)}
+    };
+});
+
+Reader.prototype.addReaderMacro('opening_paren', /^\(/, function(reader){
+    //    console.log(reader);
+    var closing_paren = new reader.Symbol(')');
+    return function(input_string, reader){
         var tail = input_string.substring(1);
         var element = {};
         var value = reader.emptyList();
         var eof = false;
         var done = false;
         while(!element.error && !done){
-            element = readFromString(tail);
-            //console.log(element);
+            element = reader.readFromString(tail);
             tail = element.tail;
-            if(symbolEquals(element.value, closing_paren)){
+            if(reader.symbolEquals(element.value, closing_paren)){
                 done = true;
             }
             else{
@@ -114,47 +124,49 @@ addReaderMacro('opening_paren', /^\(/, function(){
         }
         return done ? {value: value, tail: tail} : {tail: input_string, error: "eof"};
     };
-}());
+});
 
-addReaderMacro('number', /^[0-9]/, function(){
+Reader.prototype.addReaderMacro('number', /^[0-9]/, function(){
     var symbol_rule = /^([0-9]+\.?[0-9]*)$/;
     return function(input_string){
-        var token = searchForToken(symbol_rule, input_string);
+        var token = tokenHunter(symbol_rule, input_string);
         return token ? {value: Number(token.token), tail: token.tail} : {tail: input_string, error: "eof"};
     };
-}());
+});
 
-addReaderMacro('string', /^\"/, function(){
+Reader.prototype.addReaderMacro('string', /^\"/, function(){
     var string_rule = /^"((?:(?:[^"])|(?:\\"))*)"$/;
     return function(input_string){
-        var token = searchForToken(string_rule, input_string);
+        var token = tokenHunter(string_rule, input_string);
         return token ? {value: token.token, tail: token.tail} : {tail: input_string, error: "eof"};
     };
-}());
+});
 
-addReaderMacro('symbol', /^[^\s\(\)\'\#\[\]\{\}]/, function(){
+Reader.prototype.addReaderMacro('symbol', /^[^\s\(\)\'\#\[\]\{\}]/, function(reader){
     var symbol_rule = /^([^\s\(\)\'\#\[\]\{\}]+)$/;
     return function(input_string){
-        var token = searchForToken(symbol_rule, input_string);
-        return token ? {value: new Symbol(token.token), tail: token.tail} : {tail: input_string, error: "eof"};
+        var token = tokenHunter(symbol_rule, input_string);
+        return token ? {value: new reader.Symbol(token.token), tail: token.tail} : {tail: input_string, error: "eof"};
     };
-}());
+});
 
-var readFromString = reader.innerReadFromString = function(input_string){
-    for (var i in reader_macros){
-        var reader_macro = reader_macros[i];
+Reader.prototype.readFromString = function(input_string){
+    for (var i in this.reader_macros){
+        var reader_macro = this.reader_macros[i];
         if (reader_macro.rule.test(input_string)){
-            return reader_macro.callback(input_string);
+            return reader_macro.callback(input_string, this);
         }
     };
     return {tail:input_string, error:'illegal-input'};
 };
 
-console.log(readFromString('"hello" there'));
-console.log(readFromString('1234.123123 sdfsf'));
-console.log(readFromString('1234.123.123 sdfsf'));
-console.log(readFromString('helloworld)   '));
-console.log(readFromString('    ,"hello"'));
-console.log(readFromString('(1 2 3 4 5) more'));
-console.log(readFromString('(1 2 3 4 5 more'));
-console.log(readFromString('(1 2 3 4 5 this)something'));
+var aReader = new Reader();
+//console.log(aReader.readFromString);
+//console.log(aReader.readFromString('"hello" there'));
+console.log(aReader.readFromString('1234.123123 sdfsf'));
+console.log(aReader.readFromString('1234.123.123 sdfsf'));
+console.log(aReader.readFromString('helloworld)   '));
+console.log(aReader.readFromString('    ,"hello"'));
+console.log(aReader.readFromString('(1 2 3 4 5) more'));
+console.log(aReader.readFromString('(1 2 3 4 5 more'));
+console.log(aReader.readFromString('(1 2 3 4 5 this)something'));
